05f16817e2a6067713c85ea21d72b626bf030b2d
Johan <57840332+johannasendi@users.noreply.github.com>
2021-10-20 22:06:21 +0900

    first commit

---
+++ intro/hello.js
@@ -0,0 +1,5 @@
+function sayHello(name) {
+    return "Hello " + name;
+}
+var response = sayHello("Johan");
+console.log(response);
---
+++ intro/hello.ts
@@ -0,0 +1,8 @@
+function sayHello(name: string) 
+{
+    return `Hello ${name}`
+}
+
+let response: string = sayHello("Johan");
+
+console.log(response);
